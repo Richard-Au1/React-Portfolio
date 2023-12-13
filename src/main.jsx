73acec7395b214cf0237e4997a1908384ddd45dd
Richard-Au1 <136401843+Richard-Au1@users.noreply.github.com>
2023-12-12 23:39:04 -0700

    created reactbrowserrouter

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import App from './app';
+import { ReactDOM } from 'react-dom/client';
+
+import {createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import aboutMe from './pages/aboutMe';
+import resume from './pages/resume';
+import portfolio from './pages/portfolio';
+import contact from './pages/contact';
+
+const router = createBrowserRouter([
+
+]);
