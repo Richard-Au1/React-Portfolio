6708e4dc18e18df87f28380a8fdd06494da53ec2
Richard-Au1 <136401843+Richard-Au1@users.noreply.github.com>
2023-12-12 23:43:55 -0700

    added in properties before adding in the pages to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+// importing all the imports
 import App from './app';
 import { ReactDOM } from 'react-dom/client';
 
@@ -8,6 +9,14 @@ import resume from './pages/resume';
 import portfolio from './pages/portfolio';
 import contact from './pages/contact';
 
+// creating a webrowser
 const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App/>,
+        // errorElement: <NoFound/> will add in as a later page.
+        children: [
 
+        ],
+    },
 ]);
