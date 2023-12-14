7f45ef9d35deac40933428b5d773d5b80df81dcd
Richard-Au1 <136401843+Richard-Au1@users.noreply.github.com>
2023-12-13 22:35:28 -0700

    pushing for deploying

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -3,19 +3,25 @@ export default function Portfolio() {
                 <div>
                         <section id="work" className="headers mt-2">Projects</section>
                         <div className="card-container">
-                                <div id="refactored" ><a href="https://ali-aldawoodi.github.io/Limitless-Growth/"
-                                        alt="man-with-coffee-in-hand">Refactored Code</a></div>
+                                <div id="lol" >
+                                        <a href="https://github.com/Richard-Au1/LOL-CARDS" alt="lol pic">Lol-Cards</a>
+                                </div>
                         </div>
                         <div className="top-2">
-                                <div className="card-1" id="tutor-ratings"><a href="https://github.com/Ali-Aldawoodi/Tutor-Ratings"
-                                        alt="Tutor ratings">Tutor-Ratings</a></div>
-                                <div className="card-1" id="one-click-eats"><a href="https://stephen-garcia.github.io/RecipeProject/" alt="page-with-food">One Click Eats</a></div>
+                                <div className="card-1" id="tutor-ratings">
+                                        <a href="https://github.com/Ali-Aldawoodi/Tutor-Ratings" alt="Tutor ratings">Tutor-Ratings</a>
+                                </div>
+                                <div className="card-1" id="tracker">
+                                        <a href="https://github.com/Richard-Au1/Employee-tracker" alt="employee tracker">Employee Tracker</a>
+                                </div>
                         </div>
                         <div className="bottom-2">
-                                <div className="card-2" id="weather-app"><a href="../assets/weather app.jpg" alt="image-of-weather-app">Weather
-                                        app</a></div>
-                                <div className="card-2" id="parking-app"><a href="../assets/parking app.jpg" alt="image-of-parking-app">Parking
-                                        App</a></div>
+                                <div className="card-2" id="svg">
+                                        <a href="https://github.com/Richard-Au1/SVG-Creator" alt="svg creator">Svg Creator</a>
+                                </div>
+                                <div className="card-2" id="parking-app">
+                                        <a href="../assets/parking app.jpg" alt="image-of-parking-app">Parking App</a>
+                                </div>
                         </div>
                 </div>
         )
