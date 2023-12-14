ee0d8a626b84beda7575b35a3faefe46edf2864a
Richard-Au1 <136401843+Richard-Au1@users.noreply.github.com>
2023-12-13 23:26:36 -0700

    fixed resume, contact, and portfolio page

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,7 +1,7 @@
 export default function Nav({ links }) {
     return (
       <nav>
-        <div class="container-fluid">
+        <div className="container-fluid">
             <ul style={{margin: '20px', backgroundColor: 'turquoise'}} >
               {links.map((link) => link)}
             </ul>
