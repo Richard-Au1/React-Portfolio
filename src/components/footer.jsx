2330844c0b6ffd5e21f4c311ca2010aefc0d9213
Richard-Au1 <136401843+Richard-Au1@users.noreply.github.com>
2023-12-14 00:34:25 -0700

    created linked in link and facebook

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,13 @@ export default function Footer() {
     return (
         <footer className="d-flex justify-content-center ">
             <div className='pe-3'>
+                <a href="https://www.linkedin.com/in/richard-au-9699a729b/">LinkedIn</a>
+            </div>
+            <div className='pe-3'>
                 <a href="https://github.com/Richard-Au1/React-Portfolio">Github</a>
+            </div>
+            <div className='pe-3'>
+                <a href="https://www.facebook.com/richard.au.9/">Facebook</a>
             </div>
         </footer>
     )
